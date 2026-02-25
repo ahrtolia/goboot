8d0c3dc912be3ff995fecfcafb566995e84cea14
zhaoxiaodong <xiaodong.zhao-2@yeepay.com>
2026-02-25 14:52:11 +0800

    feat: support environment variable for config file flag

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strings"
 )
 
-var configFile = flag.String("c", "config.yaml", "config file")
+type stringFlag struct {
+	value string
+	set   bool
+}
+
+func (s *stringFlag) String() string {
+	return s.value
+}
+
+func (s *stringFlag) Set(val string) error {
+	s.value = val
+	s.set = true
+	return nil
+}
 
 func main() {
 
+	configFlag := &stringFlag{value: "config.yaml"}
+	flag.Var(configFlag, "c", "config file")
 	flag.Parse()
 
-	app, err := CreateApp(*configFile)
+	configFile := configFlag.value
+	if !configFlag.set {
+		if envConfig := strings.TrimSpace(os.Getenv("CONFIG_NAME")); envConfig != "" {
+			configFile = envConfig
+		}
+	}
+
+	app, err := CreateApp(configFile)
 	if err != nil {
 		panic(err)
 	}
